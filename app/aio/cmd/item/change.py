5b56bf6f419d19f42b641ff6d1fa3ce84e9ef776
MrBlackov <madaminov.2008.mms@gmail.com>
2026-05-28 16:18:35 +0300

    + craft page to wiki

--- app/aio/cmd/item/change.py
+++ app/aio/cmd/item/change.py
@@ -3,7 +3,7 @@ from aiogram.filters import CommandStart, Command, CommandObject
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from app.logged.botlog import log
-from app.aio.config import owner
+from app.aio.config import owner, admins
 from app.service.item import ItemService
 from app.exeption.decorator import exept, call_exept
 from app.aio.cls.fsm.item import ChangeItemSketchState
@@ -25,7 +25,7 @@ change_item_router = Router()
 @log.decor(arg=True)
 @exept
 async def cmd_handler(message: Message, command: CommandObject, state: FSMContext, **kwargs):
-    if command.args != None and message.from_user.id == owner:
+    if command.args != None and message.from_user.id in admins:
         msg, markup = await ItemService(message.from_user.id, state, message).change.start(command.args)
         await message.answer(msg, reply_markup=markup)
     elif command.args == None:
